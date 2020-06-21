01b363c7be192da77a804af219cf7789b4ccffc3
toomec2105 <tomec2105@interia.pl>
2020-06-21 16:23:06 +0200

    mexican wave

---
+++ src/main/java/katas/MexicanWave.java
@@ -0,0 +1,25 @@
+package katas;
+
+import java.util.List;
+import java.util.ArrayList;
+public class MexicanWave {
+
+    public static String[] wave(String str) {
+        List < String > wave = new ArrayList();
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentLetter = str.charAt(i);
+            if (currentLetter != ' ') {
+
+                StringBuilder textToBeWaved = new StringBuilder(str);
+                textToBeWaved.setCharAt(i, Character.toUpperCase(currentLetter));
+                wave.add(textToBeWaved.toString());
+            }
+
+        }
+
+        String[] arr = wave.toArray(new String[] {});
+        return arr;
+    }
+
+}
