80e4f63f0ab94da9e8d40aae659a49e4c7c70a1b
toomec2105 <tomec2105@interia.pl>
2020-06-25 17:35:03 +0200

    make rrecursive version work for some tests for zoo disaster

--- src/main/java/katas/zooDisaster.java
+++ src/main/java/katas/zooDisaster.java
@@ -6,77 +6,124 @@ import java.util.Set;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
+
 public class zooDisaster {
 
-  public static String[] whoEatsWho(final String zoo) {
-    ArrayList<String> output = new ArrayList<String>();
-    output.add(zoo);
-    LinkedList<String> animals = new LinkedList<String>(Arrays.asList(zoo.split(",")));
-   
-      
-    
-    
-    for(int i = 0; i < animals.size(); i++){
-    if(i < 0){
-      continue;
-    }
-    
-      String predator = animals.get(i);
-      int previousAnimal = i - 1; 
-      if(i > 0 && isEatable(predator, animals.get(previousAnimal))){
-      
-        output.add(predator + " eats " + animals.get(previousAnimal));
-        animals.remove(previousAnimal);
-        i = i - 3;
-        
-        continue;
-      }
-      
-      int nextAnimal = i + 1;
-      
-      if(i < animals.size() - 1 && isEatable(predator, animals.get(nextAnimal))){
-      
-        output.add(predator + " eats " + animals.get(nextAnimal));
-        animals.remove(nextAnimal);
-        i = i - 1;
-      }
-      
-    }
-    output.add(createFinalZoo(animals));
-    
-    return output.toArray(new String[] {});
-  }
-  public static String createFinalZoo(LinkedList<String> animals) {
-    String finalZoo = "";
-    
-      for(int i = 0; i < animals.size(); i++){
-        finalZoo += animals.get(i);
-        finalZoo += i == animals.size()-1 ? "" : ",";
-      
-      }
-    return finalZoo;
-  }
-  public static boolean isEatable(String predator, String victim) {
-    HashMap<String, HashSet<String>> animalFoodsMap = new HashMap<String, HashSet<String>>(); 
-    
-    animalFoodsMap.put("antelope", new HashSet<>(Arrays.asList(new String[] {"grass"})));
-    animalFoodsMap.put("big-fish", new HashSet<>(Arrays.asList(new String[] {"little-fish"})));
-    animalFoodsMap.put("bug", new HashSet<>(Arrays.asList(new String[] {"leaves"})));
-    animalFoodsMap.put("bear", new HashSet<>(Arrays.asList(new String[] {"bug", "big-fish", "chicken", "cow", "leaves", "sheep"})));
-    animalFoodsMap.put("chicken", new HashSet<>(Arrays.asList(new String[] {"bug"})));
-    animalFoodsMap.put("cow", new HashSet<>(Arrays.asList(new String[] {"grass"})));
-    animalFoodsMap.put("fox", new HashSet<>(Arrays.asList(new String[] {"chicken", "sheep"})));
-    animalFoodsMap.put("giraffe", new HashSet<>(Arrays.asList(new String[] {"leaves"})));
-    animalFoodsMap.put("lion", new HashSet<>(Arrays.asList(new String[] {"antelope", "cow"})));
-    animalFoodsMap.put("panda", new HashSet<>(Arrays.asList(new String[] {"leaves"})));
-    animalFoodsMap.put("sheep", new HashSet<>(Arrays.asList(new String[] {"grass"})));
-    
-    Set<String> foods = animalFoodsMap.get(predator);
-    if(foods != null){
-      return foods.contains(victim);
-    }
-    
-    return false;
-  }
-  
+	public static String[] whoEatsWho(final String zoo) {
+		ArrayList<String> output = new ArrayList<String>();
+		output.add(zoo);
+		LinkedList<String> animals = new LinkedList<String>(Arrays.asList(zoo.split(",")));
+
+		for (int i = 0; i < animals.size(); i++) {
+			if (i < 0) {
+				continue;
+			}
+
+			String predator = animals.get(i);
+			int previousAnimal = i - 1;
+			if (i > 0 && isEatable(predator, animals.get(previousAnimal))) {
+
+				output.add(predator + " eats " + animals.get(previousAnimal));
+				animals.remove(previousAnimal);
+				i = i - 3;
+
+				continue;
+			}
+
+			int nextAnimal = i + 1;
+
+			if (i < animals.size() - 1 && isEatable(predator, animals.get(nextAnimal))) {
+
+				output.add(predator + " eats " + animals.get(nextAnimal));
+				animals.remove(nextAnimal);
+				i = i - 1;
+			}
+
+		}
+		output.add(String.join(",", animals));
+
+		return output.toArray(new String[] {});
+	}
+
+	public static boolean isEatable(String predator, String victim) {
+		HashMap<String, HashSet<String>> animalFoodsMap = new HashMap<String, HashSet<String>>();
+
+		animalFoodsMap.put("antelope", new HashSet<>(Arrays.asList(new String[] { "grass" })));
+		animalFoodsMap.put("big-fish", new HashSet<>(Arrays.asList(new String[] { "little-fish" })));
+		animalFoodsMap.put("bug", new HashSet<>(Arrays.asList(new String[] { "leaves" })));
+		animalFoodsMap.put("bear",
+				new HashSet<>(Arrays.asList(new String[] { "bug", "big-fish", "chicken", "cow", "leaves", "sheep" })));
+		animalFoodsMap.put("chicken", new HashSet<>(Arrays.asList(new String[] { "bug" })));
+		animalFoodsMap.put("cow", new HashSet<>(Arrays.asList(new String[] { "grass" })));
+		animalFoodsMap.put("fox", new HashSet<>(Arrays.asList(new String[] { "chicken", "sheep" })));
+		animalFoodsMap.put("giraffe", new HashSet<>(Arrays.asList(new String[] { "leaves" })));
+		animalFoodsMap.put("lion", new HashSet<>(Arrays.asList(new String[] { "antelope", "cow" })));
+		animalFoodsMap.put("panda", new HashSet<>(Arrays.asList(new String[] { "leaves" })));
+		animalFoodsMap.put("sheep", new HashSet<>(Arrays.asList(new String[] { "grass" })));
+
+		Set<String> foods = animalFoodsMap.get(predator);
+		if (foods != null) {
+			return foods.contains(victim);
+		}
+
+		return false;
+	}
+
+	public static String[] whoEatsWhoRecursive(final String zoo) {
+		List<String> output = new LinkedList<String>(); 
+		output.add(zoo);
+		
+		LinkedList<String> animals = new LinkedList<String>(Arrays.asList(zoo.split(",")));
+
+		List<String> zooEvents = whoEatsWhoRecursiveInternal(animals, 0, new LinkedList<String>());
+		output.addAll(zooEvents);
+		return output.toArray(new String[] {});
+	}
+
+	private static List<String> whoEatsWhoRecursiveInternal(final List<String> animals, final int curIndex, final List<String> output) {
+		
+		System.out.println("----------------------------Enter recursive internal");
+		System.out.println("animals: " + animals.toString());
+		System.out.println( " output: " + output);
+		// stop condition(when to return)
+		int nextAnimal = curIndex + 1;
+		
+		if (curIndex >= animals.size() || animals.size() == 1 || nextAnimal >= animals.size()) {
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!! Entered the Stop condition");
+			output.add(String.join(",", animals));
+			return output;
+		}
+		// logic which will call whoEatsWhoRecursiveInternal
+		//whoEatsWhoRecursiveInternal(animals, curIndex + 1, output);
+				
+		int newIndex = curIndex;
+		
+		// eats to the left
+		int previousAnimal = curIndex - 1;
+		/*
+		 * if(previousAnimal < 0) { whoEatsWhoRecursiveInternal(animals, curIndex + 1,
+		 * output); }
+		 */
+		String predator = animals.get(curIndex);
+		if(previousAnimal >= 0 && isEatable(predator, animals.get(previousAnimal))) {			
+			System.out.println("<<<<<<<<  Entered eatLeft condition");
+			output.add(predator + " eats " + animals.get(previousAnimal));
+			animals.remove(previousAnimal);
+			newIndex = curIndex -2;
+		}
+		
+		// eats to the right without eating to the left
+		if(nextAnimal < animals.size() && isEatable(predator, animals.get(nextAnimal))) {		
+			System.out.println("<<<<<<<<  Entered eatRight condition");
+			output.add(predator + " eats " + animals.get(nextAnimal));
+			animals.remove(nextAnimal);
+			newIndex = curIndex;
+		}
+		
+		System.out.println("<><><><><><><><><><><>  Doesn;t eat left or right");
+		whoEatsWhoRecursiveInternal(animals, newIndex, output);
+		return output;
+	}
+
 }
--- src/test/java/katas/zooDisasterTest.java
+++ src/test/java/katas/zooDisasterTest.java
@@ -2,22 +2,98 @@ package katas;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class zooDisasterTest {
 
-  @Test
-  public void example() {    
-    final String input = "fox,bug,chicken,grass,sheep";
-    final String[] expected = 	{
-      "fox,bug,chicken,grass,sheep", 
-      "chicken eats bug", 
-      "fox eats chicken", 
-      "sheep eats grass", 
-      "fox eats sheep", 
-      "fox"};
-    assertArrayEquals(expected, zooDisaster.whoEatsWho(input));
-  }
+	/*
+	 * @Test public void example() { final String input =
+	 * "fox,bug,chicken,grass,sheep"; final String[] expected = {
+	 * "fox,bug,chicken,grass,sheep", "chicken eats bug", "fox eats chicken",
+	 * "sheep eats grass", "fox eats sheep", "fox"}; assertArrayEquals(expected,
+	 * zooDisaster.whoEatsWho(input)); }
+	 */
+	@Test
+	public void singleElement() {
+		final String input = "sheep";
+		final String[] expected = { "sheep", "sheep" };
+		assertArrayEquals(expected, zooDisaster.whoEatsWho(input));
+	}
+
+	/*
+	 * @Test public void eatsRightOneElement() { final String input = "fox,chicken";
+	 * final String[] expected = { "fox,chicken", "fox eats chicken", "fox"};
+	 * assertArrayEquals(expected, zooDisaster.whoEatsWho(input)); }
+	 * 
+	 * @Test public void eatsRightTwoElement() { final String input =
+	 * "fox,chicken,sheep"; final String[] expected = { "fox,chicken,sheep",
+	 * "fox eats chicken", "fox eats sheep", "fox"}; assertArrayEquals(expected,
+	 * zooDisaster.whoEatsWho(input)); }
+	 * 
+	 * @Test public void eatsLeftOneElement() { final String input =
+	 * "big-fish,bear"; final String[] expected = { "big-fish,bear",
+	 * "bear eats big-fish", "bear"}; assertArrayEquals(expected,
+	 * zooDisaster.whoEatsWho(input)); }
+	 * 
+	 * @Test public void eatsLeftTwoElement() { final String input =
+	 * "little-fish,little-fish,big-fish"; final String[] expected = {
+	 * "little-fish,little-fish,big-fish", "big-fish eats little-fish",
+	 * "big-fish eats little-fish", "big-fish"}; assertArrayEquals(expected,
+	 * zooDisaster.whoEatsWho(input)); }
+	 */
+
+	/*
+	 * @Test public void example() { final String input =
+	 * "fox,bug,chicken,grass,sheep"; final String[] expected = {
+	 * "fox,bug,chicken,grass,sheep", "chicken eats bug", "fox eats chicken",
+	 * "sheep eats grass", "fox eats sheep", "fox"}; assertArrayEquals(expected,
+	 * zooDisaster.whoEatsWho(input)); }
+	 */
+	@Test
+	public void singleElementRecursive() {
+		final String input = "sheep";
+		final String[] expected = { "sheep", "sheep" };
+		assertArrayEquals(expected, zooDisaster.whoEatsWhoRecursive(input));
+	}
+
+	@Test
+	public void eatsRightOneElementRecursive() {
+		final String input = "fox,chicken";
+		final String[] expected = { "fox,chicken", "fox eats chicken", "fox" };
+		System.out.println("actual: " + Arrays.deepToString(zooDisaster.whoEatsWhoRecursive(input)));
+		assertArrayEquals(expected, zooDisaster.whoEatsWhoRecursive(input));
+	}
+
+	@Test
+	public void eatsRightTwoElementRecursive() {
+		final String input = "fox,chicken,sheep";
+		final String[] expected = { "fox,chicken,sheep", "fox eats chicken","fox eats sheep","fox" };
+		assertArrayEquals(expected, zooDisaster.whoEatsWhoRecursive(input));
+	}
+
+	@Test
+	public void eatsLeftAtIndexFourRecursive() {
+		final String input = "bug,bug,lion,chicken,fox,sheep";
+		final String[] expected = { "bug,bug,lion,chicken,fox,sheep", "fox eats chicken", "fox eats sheep",
+				"bug,bug,lion,fox" };
+		assertArrayEquals(expected, zooDisaster.whoEatsWhoRecursive(input));
+	}
+
+	/*
+	 * @Test public void eatsLeftOneElementRecursive() { final String input =
+	 * "big-fish,bear"; final String[] expected = { "big-fish,bear",
+	 * "bear eats big-fish", "bear"}; assertArrayEquals(expected,
+	 * zooDisaster.whoEatsWhoRecursive(input)); }
+	 */
+	/*
+	 * @Test public void eatsLeftTwoElementRecursive() { final String input =
+	 * "little-fish,little-fish,big-fish"; final String[] expected = {
+	 * "little-fish,little-fish,big-fish", "big-fish eats little-fish",
+	 * "big-fish eats little-fish", "big-fish"}; assertArrayEquals(expected,
+	 * zooDisaster.whoEatsWhoRecursive(input)); }
+	 */
 
 }
