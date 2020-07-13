d67a4e9bf7d90ef0506a52403848d7da84eb93c6
toomec2105 <tomec2105@interia.pl>
2020-07-13 19:28:54 +0200

    sort for loop

--- src/main/java/katas/Sort.java
+++ src/main/java/katas/Sort.java
@@ -4,10 +4,27 @@ import java.util.List;
 
 public class Sort {
 	public static List<Integer> sort(final List<Integer> input) {
-		if(input.size() == 0) {
+		if (input.size() == 0 || input.size() == 1) {
 			return input;
 		}
-		return input;
 		
+		List<Integer> numbers = input;
+
+		for (int i = 1; i < numbers.size(); i++) {
+			
+			int current = numbers.get(i);
+			int previous = numbers.get(i - 1);
+			
+			
+			if (current < previous) {
+				
+				numbers.set(i, previous);
+				numbers.set(i - 1, current);
+				i = i > 1 ? i - 2 : 0;
+			}
+		}
+
+		return numbers;
+
 	}
 }
--- src/test/java/katas/SortingTest.java
+++ src/test/java/katas/SortingTest.java
@@ -9,28 +9,35 @@ import org.junit.jupiter.api.Test;
 class SortingTest {
 	@Test
 	void givenEmptyList_returnsEmptyList() {
-		 assertEquals("Expected:  Actual: ", Sort.sort(Arrays.asList(0)));
+		 assertEquals(Arrays.asList(), Sort.sort(Arrays.asList()));
 	}
-	/*
-	 * @Test void givenOneElement_sortsCorrectly() {
-	 * assertEquals("Expected: 8 Actual: ", Sort.sort(Arrays.asList(8))); }
-	 * 
-	 * @Test void givenTwoElements_sortsCorrectly() {
-	 * assertEquals("Expected: 2,6 Actual: ", Sort.sort(Arrays.asList(6,2))); }
-	 * 
-	 * @Test void givenMultipleElements_sortsCorrectly() {
-	 * assertEquals("Expected: -7,-5,1,2,3,4,6,8 Actual: ",
-	 * Sort.sort(Arrays.asList(8,2,-5,1,-7,6,3,4))); }
-	 * 
-	 * @Test void givenMultipleElementsWithDuplicates_sortsCorrectly() {
-	 * assertEquals("Expected: -4,5,8,8,11,11 Actual: ",
-	 * Sort.sort(Arrays.asList(8,8,5,11,-4,11))); }
-	 * 
-	 * @Test void givenTwoElementsWithDuplicates_sortsCorrectly() {
-	 * assertEquals("Expected: 2,2 Actual: ", Sort.sort(Arrays.asList(2,2))); }
-	 * 
-	 * @Test void givenMultipleElementsWithThreeDuplicatesInRow_sortsCorrectly() {
-	 * assertEquals("Expected: -2,1,3,5,7,7,7,8 Actual: ",
-	 * Sort.sort(Arrays.asList(5,3,7,7,7,-2,8,1))); }
-	 */
+	
+	
+	  @Test void givenOneElement_sortsCorrectly() { assertEquals(Arrays.asList(8),
+	  Sort.sort(Arrays.asList(8))); }
+	  
+		
+		  @Test void givenTwoElements_sortsCorrectly() {
+		  assertEquals(Arrays.asList(2,6), Sort.sort(Arrays.asList(6,2))); }
+		  
+		
+		  @Test void givenMultipleElements_sortsCorrectly() {
+		  assertEquals(Arrays.asList(-7,-5,1,2,3,4,6,8),
+		  Sort.sort(Arrays.asList(8,2,-5,1,-7,6,3,4))); }
+		  
+		
+		  @Test void givenMultipleElementsWithDuplicates_sortsCorrectly() {
+		  assertEquals(Arrays.asList(-4,5,8,8,11,11),
+		  Sort.sort(Arrays.asList(8,8,5,11,-4,11))); }
+		  
+		  @Test void givenTwoElementsWithDuplicates_sortsCorrectly() {
+		  assertEquals(Arrays.asList(2,2), Sort.sort(Arrays.asList(2,2))); }
+		  
+		  @Test void givenMultipleElementsWithThreeDuplicatesInRow_sortsCorrectly() {
+		  assertEquals(Arrays.asList(-2,1,3,5,7,7,7,8),
+		  Sort.sort(Arrays.asList(5,3,7,7,7,-2,8,1))); }
+		 
+		 
+	 
+	 
 }
