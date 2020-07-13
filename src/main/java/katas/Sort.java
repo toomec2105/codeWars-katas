cb32e342800066696f8cca0c3738889f87fbe29c
toomec2105 <tomec2105@interia.pl>
2020-07-13 19:08:38 +0200

    do tests for Sort

---
+++ src/main/java/katas/Sort.java
@@ -0,0 +1,13 @@
+package katas;
+
+import java.util.List;
+
+public class Sort {
+	public static List<Integer> sort(final List<Integer> input) {
+		if(input.size() == 0) {
+			return input;
+		}
+		return input;
+		
+	}
+}
