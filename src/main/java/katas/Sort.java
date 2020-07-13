5099ce3b91364970dce5f1fd0eb2a9c1e9ba27a1
toomec2105 <tomec2105@interia.pl>
2020-07-13 20:08:56 +0200

    slection & insertion sort

--- src/main/java/katas/Sort.java
+++ src/main/java/katas/Sort.java
@@ -1,30 +1,69 @@
 package katas;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sort {
-	public static List<Integer> sort(final List<Integer> input) {
+	public static List<Integer> insertionSort(final List<Integer> input) {
 		if (input.size() == 0 || input.size() == 1) {
 			return input;
 		}
 		
-		List<Integer> numbers = input;
+		List<Integer> numbers = new ArrayList<>(input);
 
 		for (int i = 1; i < numbers.size(); i++) {
 			
 			int current = numbers.get(i);
-			int previous = numbers.get(i - 1);
 			
-			
-			if (current < previous) {
+			for (int j = i; j > 0; j--) {
+				int previous = numbers.get(j - 1);
 				
-				numbers.set(i, previous);
-				numbers.set(i - 1, current);
-				i = i > 1 ? i - 2 : 0;
+				if (current < previous) {
+					
+					numbers.set(j, previous); 
+					numbers.set(j-1, current);
+				}else {
+					break;
+				}
 			}
+			
+			
+			/*
+			 * if (current < previous) {
+			 * 
+			 * numbers.set(i, previous); numbers.set(i - 1, current); i = i > 1 ? i - 2 : 0;
+			 * }
+			 */
 		}
 
 		return numbers;
 
 	}
+	
+	public static List<Integer> selectionSort(final List<Integer> input) {
+		if (input.size() == 0 || input.size() == 1) {
+			return input;
+		}
+		
+		List<Integer> numbers = new ArrayList<>(input);
+		List<Integer> output = new ArrayList<>();
+
+		while(numbers.size() > 0) {
+			
+			Integer smallestNum = numbers.get(0);
+			int smallestNumIndex = 0;
+			
+			for(int j = 0; j < numbers.size(); j++) {
+				smallestNum = numbers.get(j) <= smallestNum ? numbers.get(j) : smallestNum;
+				smallestNumIndex = numbers.get(j) <= smallestNum ? j : smallestNumIndex;
+			}
+			
+			output.add(smallestNum);
+			numbers.remove(smallestNumIndex);
+		}
+		return output;
+		
+	}
+	
+	
 }
--- src/test/java/katas/SortingTest.java
+++ src/test/java/katas/SortingTest.java
@@ -9,35 +9,65 @@ import org.junit.jupiter.api.Test;
 class SortingTest {
 	@Test
 	void givenEmptyList_returnsEmptyList() {
-		 assertEquals(Arrays.asList(), Sort.sort(Arrays.asList()));
+		 assertEquals(Arrays.asList(), Sort.insertionSort(Arrays.asList()));
 	}
 	
 	
 	  @Test void givenOneElement_sortsCorrectly() { assertEquals(Arrays.asList(8),
-	  Sort.sort(Arrays.asList(8))); }
+	  Sort.insertionSort(Arrays.asList(8))); }
 	  
 		
 		  @Test void givenTwoElements_sortsCorrectly() {
-		  assertEquals(Arrays.asList(2,6), Sort.sort(Arrays.asList(6,2))); }
+		  assertEquals(Arrays.asList(2,6), Sort.insertionSort(Arrays.asList(6,2))); }
 		  
 		
 		  @Test void givenMultipleElements_sortsCorrectly() {
 		  assertEquals(Arrays.asList(-7,-5,1,2,3,4,6,8),
-		  Sort.sort(Arrays.asList(8,2,-5,1,-7,6,3,4))); }
+		  Sort.insertionSort(Arrays.asList(8,2,-5,1,-7,6,3,4))); }
 		  
 		
 		  @Test void givenMultipleElementsWithDuplicates_sortsCorrectly() {
 		  assertEquals(Arrays.asList(-4,5,8,8,11,11),
-		  Sort.sort(Arrays.asList(8,8,5,11,-4,11))); }
+		  Sort.insertionSort(Arrays.asList(8,8,5,11,-4,11))); }
 		  
 		  @Test void givenTwoElementsWithDuplicates_sortsCorrectly() {
-		  assertEquals(Arrays.asList(2,2), Sort.sort(Arrays.asList(2,2))); }
+		  assertEquals(Arrays.asList(2,2), Sort.insertionSort(Arrays.asList(2,2))); }
 		  
 		  @Test void givenMultipleElementsWithThreeDuplicatesInRow_sortsCorrectly() {
 		  assertEquals(Arrays.asList(-2,1,3,5,7,7,7,8),
-		  Sort.sort(Arrays.asList(5,3,7,7,7,-2,8,1))); }
+		  Sort.insertionSort(Arrays.asList(5,3,7,7,7,-2,8,1))); }
 		 
-		 
+		 //---------------------------------------------------
+		  
+			@Test
+			void givenEmptyList_2returnsEmptyList() {
+				 assertEquals(Arrays.asList(), Sort.selectionSort(Arrays.asList()));
+			}
+			
+			
+			  @Test void givenOneElement_sorts2Correctly() { assertEquals(Arrays.asList(8),
+			  Sort.selectionSort(Arrays.asList(8))); }
+			  
+				
+				  @Test void givenTwoElements_sorts2Correctly() {
+				  assertEquals(Arrays.asList(2,6), Sort.selectionSort(Arrays.asList(6,2))); }
+				  
+				
+				  @Test void givenMultipleElements_sorts2Correctly() {
+				  assertEquals(Arrays.asList(-7,-5,1,2,3,4,6,8),
+				  Sort.selectionSort(Arrays.asList(8,2,-5,1,-7,6,3,4))); }
+				  
+				
+				  @Test void givenMultipleElementsWithDuplicates_sorts2Correctly() {
+				  assertEquals(Arrays.asList(-4,5,8,8,11,11),
+				  Sort.selectionSort(Arrays.asList(8,8,5,11,-4,11))); }
+				  
+				  @Test void givenTwoElementsWithDuplicates_sorts2Correctly() {
+				  assertEquals(Arrays.asList(2,2), Sort.selectionSort(Arrays.asList(2,2))); }
+				  
+				  @Test void givenMultipleElementsWithThreeDuplicatesInRow_sorts2Correctly() {
+				  assertEquals(Arrays.asList(-2,1,3,5,7,7,7,8),
+				  Sort.selectionSort(Arrays.asList(5,3,7,7,7,-2,8,1))); }
 	 
 	 
 }
